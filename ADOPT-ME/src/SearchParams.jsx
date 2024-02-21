e908ed29a6ab5182602b6f295b714c53111c288e
vazquezea96 <vazquezeduardo1278@gmail.com>
2024-02-20 23:42:37 -0800

    Completed React Query portion

--- ADOPT-ME/src/SearchParams.jsx
+++ ADOPT-ME/src/SearchParams.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
-import Pet from "./Pet";
+import Results from "./Results";
+import useBreedList from "./useBreedList";
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
@@ -7,7 +8,7 @@ const SearchParams = () => {
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
   const [pets, setPets] = useState([]);
-  const breeds = [];
+  const [breeds] = useBreedList(animal);
 
   useEffect(() => {
     requestPets();
@@ -74,14 +75,7 @@ const SearchParams = () => {
           <button>Submit</button>
         </label>
       </form>
-      {pets.map((pet) => (
-        <Pet
-          name={pet.name}
-          animal={pet.animal}
-          breed={pet.breed}
-          key={pet.id}
-        />
-      ))}
+      <Results pets={pets} />
     </div>
   );
 };
